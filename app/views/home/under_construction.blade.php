250e63c667882820443537e92af87eca8f68a6ac
Noel De Martin <noeldemartin@gmail.com>
2014-10-08 10:37:16 +0200

    #4 Add Under Construction page for desktop

--- app/views/home/under_construction.blade.php
+++ app/views/home/under_construction.blade.php
@@ -1,5 +1,56 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-@section('content')
-	<h1>Under Construction</h1>
-@stop
+		<title>Noel De Martin - Under Construction</title>
+
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<style type="text/css">
+			body, html {
+				height: 100%;
+			}
+			body {
+				background: #d8d8d8;
+				background-image: url('img/bits.png');
+				margin: 0;
+			}
+			#under-construction {
+				max-width: 1200px;
+				height: 750px;
+				margin: auto;
+				position: relative;
+				margin-top: -5%;
+			}
+			#under-construction h1 {
+				font-family: Verdana, Geneva, sans-serif;
+				font-size: 130px;
+				position: absolute;
+				left: 0;
+				bottom: 0;
+				margin: 0;
+			}
+			#under-construction img {
+				position: absolute;
+				right: 0;
+				top: 0;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div style="display: table;height: 100%; width: 100%;">
+			<div style="display: table-cell; vertical-align: middle;">
+				<div id="under-construction">
+					<h1>Under <br> Construction</h1>
+					<img src="img/under-construction.png" width="700px">
+					@include('assets.twitter')
+				</div>
+			</div>
+		</div>
+		@include('assets.analytics')
+	</body>
+</html>
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -11,6 +11,8 @@
 
 		<!-- stylesheets -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		@yield('styles')
 	</head>
 
 	<body>
@@ -45,14 +47,6 @@
 
 		@yield('scripts')
 
-        <!-- Google Analytics -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
+		@include('assets.analytics')
 	</body>
 </html>
