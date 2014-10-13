14955d3f10b5b7d8b268b1b90a93e2784b6c0c0f
Noel De Martin <noeldemartin@gmail.com>
2014-10-13 18:09:16 +0200

    #4 Fix CSS styling for under construction page

---
+++ app/views/assets/linkedin.php
@@ -0,0 +1,3 @@
+<a href="http://es.linkedin.com/pub/noel-de-martin-fernandez/41/a7b/64">
+	<img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" width="160" height="33" border="0" alt="View Noel De Martin Fernandez's profile on LinkedIn">
+</a>
--- app/views/home/under_construction.blade.php
+++ app/views/home/under_construction.blade.php
@@ -9,70 +9,30 @@
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<style type="text/css">
-			body, html {
-				height: 100%;
-			}
-			body {
-				background: #d8d8d8;
-				background-image: url('img/bits.png');
-				margin: 0;
-			}
-			#under-construction {
-				max-width: 1200px;
-				height: 750px;
-				margin: auto;
-				position: relative;
-				margin-top: -5%;
-			}
-			#under-construction h1 {
-				font-family: Verdana, Geneva, sans-serif;
-				font-size: 130px;
-				position: absolute;
-				left: 0;
-				bottom: 0;
-				margin: 0;
-			}
-			#under-construction img {
-				position: absolute;
-				right: 0;
-				top: 0;
-			}
-			#alert {
-				position: absolute;
-				left: 5%;
-				right: 5%;
-				top: 2%;
-				color: #8A6D3B;
-				background-color: #FCF8E3;
-				padding: 15px;
-				margin-bottom: 20px;
-				border: 1px solid #FAEBCC;
-				border-radius: 4px;
-				z-index: 10;
-			}
-			a {
-				color: #428BCA;
-				text-decoration: none;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="css/under-construction.css">
 	</head>
 
 	<body>
-		@if (Auth::check() && Auth::user()->isReviewer())
-			<div id="alert">
-				Hello Reviewer! You can check which posts are pending <b>{{ HTML::linkRoute('posts.index', 'here') }}</b>. Cheers, and thanks for contributing.
-			</div>
-		@endif
-		<div style="display: table;height: 100%; width: 100%;">
-			<div style="display: table-cell; vertical-align: middle;">
-				<div id="under-construction">
-					<h1>Under <br> Construction</h1>
-					<img src="img/under-construction.png" width="700px">
-					@include('assets.twitter')
+
+		<div id="top">
+			@if (Auth::check() && Auth::user()->isReviewer())
+				<div id="alert">
+					Hello Reviewer! You can check which posts are pending <b>{{ HTML::linkRoute('posts.index', 'here') }}</b>. Cheers, and thanks for contributing.
 				</div>
-			</div>
+			@endif
+			@include('assets.twitter')
+			@include('assets.linkedin')
 		</div>
+
+		<table id="under-construction">
+			<tr><td>
+				<img src="img/under-construction.png">
+			</td></tr>
+		</table>
+
+		<h1 id="title">Under<br>Construction</h1>
+
 		@include('assets.analytics')
+
 	</body>
 </html>
