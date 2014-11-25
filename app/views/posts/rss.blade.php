4dff0109cd2afea9399daaf68296b05675ef77f9
Noel De Martin <noeldemartin@gmail.com>
2014-11-25 10:29:01 +0100

    #17 Fix errors in rss.xml

--- app/views/posts/rss.blade.php
+++ app/views/posts/rss.blade.php
@@ -3,16 +3,16 @@
 	<channel>
 		<title>Noel De Martin Blog</title>
 		<link>{{ url('blog') }}</link>
-		<atom:link href="{{ url('blog.rss') }}" rel="self" type="application/rss+xml" />
+		<atom:link href="{{ route('blog.rss') }}" rel="self" type="application/rss+xml" />
 		<description>Personal Blog of Noel De Martin - Developer Entrepreneur</description>
 		<language>en-us</language>
-		<webMaster>noeldemartin@gmail.com</webMaster>
+		<webMaster>noeldemartin@gmail.com (Noel De Martin)</webMaster>
 		@foreach($posts as $post)
 			<item>
 				<title>{{ $post->title }}</title>
 				<link>{{ route('posts.show', $post->tag) }}</link>
 				<guid>{{ route('posts.show', $post->tag) }}</guid>
-				<pubDate>{{ $post->published_at->format(DateTime::RFC822) }}</pubDate>
+				<pubDate>{{ $post->published_at->format(DateTime::RSS) }}</pubDate>
 				<author>noeldemartin@gmail.com (Noel De Martin)</author>
 			</item>
 		@endforeach
