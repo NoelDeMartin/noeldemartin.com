7e0ea977346211683ac97f8f7c7187a86257bbcb
Noel De Martin <noeldemartin@gmail.com>
2014-11-25 09:32:57 +0100

    #16 remove colons in post comment email

--- app/views/emails/post_comment.blade.php
+++ app/views/emails/post_comment.blade.php
@@ -1,3 +1,3 @@
-<p>You have got a new comment in your blog! The post which was commented was "{{ HTML::linkRoute('posts.show', $post->title, $post->tag) }}". Take a look:</p>
+<p>You have got a new comment in your blog! The post which was commented was {{ HTML::linkRoute('posts.show', $post->title, $post->tag) }}. Take a look:</p>
 
 <blockquote>{{ nl2br($comment->text) }}</blockquote> -- <cite>{{ $comment->author_link? HTML::link($comment->author_link, $comment->author) : $comment->author }}</cite>
