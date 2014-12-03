84fa7b2a1e1115f5148349b54c3464f439d5d17c
Noel De Martin <noeldemartin@gmail.com>
2014-12-03 16:39:23 +0100

    #20 Add link with tag for social share

--- app/libraries/Social.php
+++ app/libraries/Social.php
@@ -5,20 +5,20 @@ class Social {
 	public static function tweetLink(Post $post) {
 		return 'https://twitter.com/intent/tweet' .
 					'?text=' . urlencode(utf8_encode('"' . $post->title . '" by @NoelDeMartin')) .
-					'&url=' . urlencode(route('posts.show', $post->id));
+					'&url=' . urlencode(route('posts.show', $post->tag));
 	}
 
 	public static function facebookShareLink(Post $post) {
-		return 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode(route('posts.show', $post->id));
+		return 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode(route('posts.show', $post->tag));
 	}
 
 	public static function googlePlusShareLink(Post $post) {
-		return 'https://plus.google.com/share?url=' . urlencode(route('posts.show', $post->id));
+		return 'https://plus.google.com/share?url=' . urlencode(route('posts.show', $post->tag));
 	}
 
 	public static function mailShareLink(Post $post) {
 		return 'mailto:?subject=' . rawurlencode(utf8_encode($post->title)) .
-						'&body=' . rawurlencode('Check out "' . $post->title . '" by Noel De Martin: ' . route('posts.show', $post->id));
+						'&body=' . rawurlencode('Check out "' . $post->title . '" by Noel De Martin: ' . route('posts.show', $post->tag));
 	}
 
 }
