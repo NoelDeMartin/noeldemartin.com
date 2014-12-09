3f083956322dc87bae9b05bb51bc44f2fdaf0e22
Noel De Martin <noeldemartin@gmail.com>
2014-12-09 12:44:55 +0100

    #21 Add freedom calculator link in experiments page

--- app/views/home/experiments.blade.php
+++ app/views/home/experiments.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 	<article id="experiments" class="readable-text">
 		<h1>Experiments</h1>
-		<p>Comming soon...</p>
+		<ul>
+			<li>{{ HTML::linkRoute('experiments.freedom-calculator', 'Freedom Calculator') }}</li>
+			<li>...</li>
+		</ul>
 	</article>
 @stop
