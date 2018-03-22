3017c3a1ec155ba143113c069d5230141f5c368d
Noel De Martin <noeldemartin@gmail.com>
2018-03-22 15:44:21 +0100

    #20 Fix migration

--- database/migrations/2018_03_22_101727_update_text_collation.php
+++ database/migrations/2018_03_22_101727_update_text_collation.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 
-// This migration has been run on mysql cli instead, for more info:
-// https://stackoverflow.com/questions/49427317/why-running-laravel-doctrine-raw-database-queries-is-not-the-same-as-using-mys
-
 class UpdateTextCollation extends Migration
 {
     /**
@@ -14,8 +11,9 @@ class UpdateTextCollation extends Migration
      */
     public function up()
     {
-        // DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_markdown LONGTEXT COLLATE utf8mb4_unicode_ci');
-        // DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_html LONGTEXT COLLATE utf8mb4_unicode_ci');
+        DB::getPdo()->prepare("set session sql_mode='NO_ENGINE_SUBSTITUTION'")->execute();
+        DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_markdown LONGTEXT COLLATE utf8mb4_unicode_ci');
+        DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_html LONGTEXT COLLATE utf8mb4_unicode_ci');
     }
 
     /**
@@ -25,7 +23,8 @@ class UpdateTextCollation extends Migration
      */
     public function down()
     {
-        // DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_markdown LONGTEXT COLLATE utf8_unicode_ci');
-        // DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_html LONGTEXT COLLATE utf8_unicode_ci');
+        DB::getPdo()->prepare("set session sql_mode='NO_ENGINE_SUBSTITUTION'")->execute();
+        DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_markdown LONGTEXT COLLATE utf8_unicode_ci');
+        DB::unprepared('ALTER TABLE posts MODIFY COLUMN text_html LONGTEXT COLLATE utf8_unicode_ci');
     }
 }
