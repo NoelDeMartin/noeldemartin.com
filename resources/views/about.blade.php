d17073d1a959798876569f5b87606aaadeb8698b
Noel De Martin <noeldemartin@gmail.com>
2018-05-15 13:23:35 +0200

    Remove 'stuff' from intro

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,9 +6,9 @@
         <h1>Hi there!</h1>
 
         <p class="text-lg">
-            My name is Noel, I am a developer and entrepreneur who loves to learn, solve problems and build
-            <i title="From fun experiments to products that make an impact">stuff</i>. I have been working on the software industry
-            since 2011 in all kinds of projects and I’m always looking for new challenges.
+            My name is Noel, I am a developer and entrepreneur who loves to learn, solve problems and build products that
+            make an impact. I have been working on the software industry since 2011 in all kinds of projects and
+            I’m always looking for new challenges.
         </p>
 
         <p class="text-lg">
