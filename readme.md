70bcaa92f2667737ad13ae64e3c116f872f7c099
Noel De Martin <noeldemartin@gmail.com>
2019-01-15 20:08:49 +0100

    Add docker files and update readme with deployment instructions

--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
 # Noel De Martin
+
+## Production
+
+### First time
+
+```sh
+scripts/init.sh
+
+docker-compose up -d
+
+docker-compose exec app npm install --only=dev
+docker-compose exec app npm run production
+docker-compose exec app php artisan migrate --force
+```
+
+### Start
+
+```sh
+docker-compose up -d
+```
+
+### Stop
+
+```
+docker-compose down
+```
---
+++ scripts/init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ -f ".env" ]; then
+   echo ".env file already exists"
+   return;
+fi
+
+cp .env.production .env
+
+APP_KEY=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c32 | base64`
+DB_PASSWORD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c32`
+
+sed -i "s/APP_KEY=/APP_KEY=base64:$APP_KEY/" .env
+sed -i "s/DB_PASSWORD=/DB_PASSWORD=$DB_PASSWORD/" .env
+
+WEB_UID=`docker-compose run app id -u www-data | sed 's/\r$//'`
+DB_UID=`docker-compose run mysql id -u mysql | sed 's/\r$//'`
+
+sudo chown -R $WEB_UID:docker .
+sudo chown -R $DB_UID:docker ./docker/mysql
