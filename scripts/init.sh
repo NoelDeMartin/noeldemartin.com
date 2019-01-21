5048ddc0147dafc1ff000cacb4dc7a040d0640d7
Noel De Martin <noeldemartin@gmail.com>
2019-01-21 21:32:42 +0100

    Update docker and nginx settings

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,6 +7,8 @@ if [ -f ".env" ]; then
    return;
 fi
 
+# Prepare .env file
+
 cp .env.production .env
 
 APP_KEY=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c32 | base64`
@@ -14,6 +16,8 @@ DB_PASSWORD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c32`
 
 sed -i "s/APP_KEY=/APP_KEY=base64:$APP_KEY/" .env
 sed -i "s/DB_PASSWORD=/DB_PASSWORD=$DB_PASSWORD/" .env
+
+# Prepare file permissions
 
 WEB_UID=`docker-compose run app id -u www-data | sed 's/\r$//'`
 DB_UID=`docker-compose run mysql id -u mysql | sed 's/\r$//'`
