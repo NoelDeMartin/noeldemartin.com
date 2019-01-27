1392e3703d3411366eeb84330dca18f712af29d4
Noel De Martin <noeldemartin@gmail.com>
2019-01-27 21:39:28 +0100

    Implement backups

---
+++ app/Providers/SpacesStorageServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Aws\S3\S3Client;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+class SpacesStorageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('spaces', function ($app, $config) {
+            $client = new S3Client([
+                'credentials' => [
+                    'key' => $config['key'],
+                    'secret' => $config['secret']
+                ],
+                'region' => $config['region'],
+                'version' => 'latest',
+                'bucket_endpoint' => false,
+                'use_path_style_endpoint' => false,
+                'endpoint' => $config['endpoint'],
+            ]);
+
+            return new Filesystem(
+                new AwsS3Adapter($client, $config['bucket'], $config['folder'])
+            );
+        });
+    }
+}
