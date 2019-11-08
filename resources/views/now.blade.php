60284f0078628b18cd5d8c07886b9ea9d6bb33dc
Noel De Martin <noeldemartin@gmail.com>
2019-11-08 12:33:30 +0100

    Add text for empty ongoing tasks

--- resources/views/now.blade.php
+++ resources/views/now.blade.php
@@ -13,22 +13,29 @@
 
     <p class="mt-2">I practice <a href="{{ url('blog/open-productivity') }}">Open Productivity</a>, and here you can see what I'm up to these days.</p>
 
-    @foreach ($tasks as $task)
-        @contentcard([
-            'url' => $task->url,
-            'title' => $task->name,
-        ])
-            {!! $task->description_html !!}
-        @endcontentcard
-    @endforeach
+    @if($tasks->isEmpty())
+        <p class="md:text-lg md:text-center">
+            It doesn't seem like I have anything going on right now! Check out the
+            <a href="{{ route('tasks.index') }}">previous tasks</a>.
+        </p>
+    @else
+        @foreach ($tasks as $task)
+            @contentcard([
+                'url' => $task->url,
+                'title' => $task->name,
+            ])
+                {!! $task->description_html !!}
+            @endcontentcard
+        @endforeach
 
-    <div class="mt-0 text-left md:text-right md:-mt-8">
-        <a href="{{ route('tasks.index') }}">
-            See previous tasks →
-        </a>
-    </div>
+        <div class="mt-0 text-left md:text-right md:-mt-8">
+            <a href="{{ route('tasks.index') }}">
+                See previous tasks →
+            </a>
+        </div>
+    @endif
 
-    <hr class="w-full mt-2 bg-grey-lighter hidden md:block">
+    <hr class="w-full mt-4 bg-grey-lighter hidden md:block">
 
     <h3>Activity</h3>
 
