43db70630a50cc2320837f2dcf144e74230e153a
Noel De Martin <noeldemartin@gmail.com>
2020-01-05 12:11:51 +0100

    Fix typo in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="p-4 bg-grey-light">
     <div class="flex flex-col max-w-content mx-auto text-sm items-center md:flex-row">
         <div class="flex items-center">
-            Build with
+            Built with
             <span class="flex items-center mr-2" title="Time, Brains & Passion">
                 @icon('time', 'w-4 h-4 ml-1')
                 @icon('brains', 'w-4 h-4 ml-1')
