65493d9771dcbce08f51a73b8c1137ffd0c684ca
Noel De Martin <noeldemartin@gmail.com>
2020-01-31 16:47:34 +0100

    Fix comment indexes in /now feeds

--- app/Models/ActivityEvent.php
+++ app/Models/ActivityEvent.php
@@ -52,20 +52,23 @@ class ActivityEvent
 
     public static function fromTaskComments($comments)
     {
+        $indexes = static::calculateCommentsTaskIndexes($comments);
+
         return collect($comments)
-            ->map(function ($comment, $index) {
-                $url = $comment->task->url . '#comment-' . ($index + 2);
+            ->map(function ($comment) use (&$indexes) {
+                $task = $comment->task;
+                $url = $task->url . '#comment-' . ($indexes[$comment->id] + 2);
 
                 return new static(
                     $comment->created_at,
                     trans('app.events.comment_posted', [
-                        'task' => "\"{$comment->task->name}\"",
+                        'task' => "\"{$task->name}\"",
                     ]),
                     trans('app.events.comment_posted', [
-                        'task' => "<a href=\"{$url}\">{$comment->task->name}</a>",
+                        'task' => "<a href=\"{$url}\">{$task->name}</a>",
                     ]),
                     trans('app.events.comment_posted_long', [
-                        'task' => "<a href=\"{$url}\">{$comment->task->name}</a>",
+                        'task' => "<a href=\"{$url}\">{$task->name}</a>",
                         'comment' => $comment->text_html,
                     ]),
                     $url
@@ -94,6 +97,30 @@ class ActivityEvent
                     $url
                 );
             });
+    }
+
+    private static function calculateCommentsTaskIndexes($comments) {
+        $taskCommentIndexes = [];
+
+        collect($comments)
+            ->sortBy('created_at')
+            ->each(function ($comment) use (&$taskCommentIndexes) {
+                $taskId = $comment->task->id;
+
+                if (!array_key_exists($taskId, $taskCommentIndexes)) {
+                    $taskCommentIndexes[$taskId] = [];
+                }
+
+                $taskCommentIndexes[$taskId][$comment->id] = count($taskCommentIndexes[$taskId]);
+            });
+
+        return array_reduce($taskCommentIndexes, function ($indexes, $comments) {
+            foreach ($comments as $commentId => $index) {
+                $indexes[$commentId] = $index;
+            }
+
+            return $indexes;
+        }, []);
     }
 
     public $date;
