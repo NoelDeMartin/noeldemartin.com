fcc3a4b8829cc96adf447631038be835c3d48e8f
Noel De Martin <noeldemartin@gmail.com>
2020-08-01 14:12:17 +0200

    Fix date header in blog page

--- resources/views/components/content-card.blade.php
+++ resources/views/components/content-card.blade.php
@@ -1,6 +1,6 @@
 <article class="content-card mb-4 md:mb-8">
 
-    <div class="flex flex-col max-w-readable md:flex-row md:justify-between">
+    <div class="title flex flex-col max-w-readable md:flex-row md:justify-between">
 
         <h2 class="my-0">
             <a
@@ -17,7 +17,10 @@
 
         @if (isset($date))
             <time
-                class="flex items-center text-blue-darker font-normal text-xs my-2 md:my-0"
+                class="
+                    flex flex-shrink-0 items-center text-blue-darker font-normal text-xs my-2
+                    md:my-0
+                "
                 datetime="{{ $date->toDateTimeString() }}"
             >
                 @icon('calendar', 'h-4 fill-current')
