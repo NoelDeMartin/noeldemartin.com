66fb25cfe3902d81b4e06af24462ae005cd1c86f
Noel De Martin <noeldemartin@gmail.com>
2021-05-25 07:38:44 +0200

    Show now feed first

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,8 +7,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="alternate" type="application/rss+xml" title="{{ trans('seo.rss') }}" href="{{ url('blog/rss.xml') }}">
         <link rel="alternate" type="application/rss+xml" title="{{ trans('seo.rss_now') }}" href="{{ url('now/rss.xml') }}">
+        <link rel="alternate" type="application/rss+xml" title="{{ trans('seo.rss') }}" href="{{ url('blog/rss.xml') }}">
 
         @semanticSEO
 
