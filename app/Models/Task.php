69c91075d01f4065ee6665370d5ae98566e11101
Noel De Martin <noeldemartin@gmail.com>
2021-07-09 19:50:30 +0200

    Fix comments after completion in TOC

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -127,8 +127,10 @@ class Task extends Model
                         $icon = blade_icon('task-started', 'w-4 h-4 mr-2');
                     } else if ($comment->created_at->eq($this->completed_at)) {
                         $icon = blade_icon('task-completed', 'w-4 h-4 mr-2');
-                    } else {
+                    } else if ($comment->created_at->lt($this->completed_at)) {
                         $icon = blade_icon('timer', 'w-4 h-4 mr-2 text-blue-darker fill-current');
+                    } else {
+                        $icon = blade_icon('checkmark', 'w-4 h-4 mr-2 text-jade-darker fill-current');
                     }
 
                     return (object) [
