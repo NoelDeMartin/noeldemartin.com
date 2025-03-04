7189a4b250fcd89a72195e4eed35f23513824075
Noel De Martin <noel@noeldemartin.com>
2025-03-04 12:02:15 +0100

    Fix anchors navigation

--- resources/assets/js/turbo/content.js
+++ resources/assets/js/turbo/content.js
@@ -1,3 +1,26 @@
+import { after } from '@noeldemartin/utils';
+
+let lastClickedUrl;
+
 document.querySelectorAll('.heading-permalink').forEach((heading) => {
     heading.dataset.turbo = false;
+});
+
+document.addEventListener('turbo:click', (event) => {
+    lastClickedUrl = event.detail.url;
+});
+
+document.addEventListener('turbo:load', async () => {
+    // Wait for header animation to finish.
+    await after({ ms: 500 });
+
+    if (
+        !lastClickedUrl ||
+        window.location.href === lastClickedUrl ||
+        !lastClickedUrl.startsWith(window.location.href)
+    ) {
+        return;
+    }
+
+    window.location.href = lastClickedUrl;
 });
