d13d43e9075c392fe8b4dfa62d5ba771c74862cd
Noel De Martin <noel@noeldemartin.com>
2026-02-12 07:27:36 +0100

    Fix back navigation

--- resources/assets/js/turbo/content.js
+++ resources/assets/js/turbo/content.js
@@ -1,6 +1,7 @@
 import { after } from '@noeldemartin/utils';
 
 let lastClickedUrl;
+let isClickNavigation = false;
 
 document.querySelectorAll('.heading-permalink').forEach((heading) => {
     heading.dataset.turbo = false;
@@ -8,9 +9,26 @@ document.querySelectorAll('.heading-permalink').forEach((heading) => {
 
 document.addEventListener('turbo:click', (event) => {
     lastClickedUrl = event.detail.url;
+    isClickNavigation = true;
+});
+
+document.addEventListener('turbo:before-visit', (event) => {
+    if (event.detail.action !== 'restore') {
+        return;
+    }
+
+    // Clear redirects on browser navigation
+    isClickNavigation = false;
+    lastClickedUrl = null;
 });
 
 document.addEventListener('turbo:load', async () => {
+    if (!isClickNavigation) {
+        lastClickedUrl = null;
+
+        return;
+    }
+
     // Wait for header animation to finish.
     await after({ ms: 500 });
 
@@ -19,8 +37,13 @@ document.addEventListener('turbo:load', async () => {
         window.location.href === lastClickedUrl ||
         !lastClickedUrl.startsWith(window.location.href)
     ) {
+        isClickNavigation = false;
+        lastClickedUrl = null;
+
         return;
     }
 
     window.location.href = lastClickedUrl;
+    isClickNavigation = false;
+    lastClickedUrl = null;
 });
