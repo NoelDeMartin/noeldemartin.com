2fb566f52aa44444958f5c0ef7f3247930cd28e9
Noel De Martin <noel@noeldemartin.com>
2026-05-07 09:41:01 +0200

    Fix 404 styles

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,15 @@
 @extends('layout', ['title' => 'Not found'])
 
 @section('main')
-    <div class="fixed inset-0 mt-12 flex items-center justify-center">
-        <a href="https://xkcd.com/1969/" alt="Not Found" target="_blank">
+    <div
+        class="pointer-events-none fixed inset-0 mt-12 flex items-center justify-center"
+    >
+        <a
+            href="https://xkcd.com/1969/"
+            alt="Not Found"
+            target="_blank"
+            class="pointer-events-auto"
+        >
             <img
                 class="max-h-full max-w-full"
                 src="https://imgs.xkcd.com/comics/not_available.png"
